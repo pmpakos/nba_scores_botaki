ade561326dce939fb8a5911f24d90ca44210acb2
pmpakos <pmpakos@hotmail.com>
2023-03-30 18:29:35 +0300

    update to bitly api (no longer reachable..found a workaround)

--- bot.py
+++ bot.py
@@ -9,28 +9,36 @@ import requests
 from selenium import webdriver
 import os
 import time
-import bitly_api
+# import bitly_api
+import requests
+import json
+import time
+import sys
 
-# CONSUMER_KEY = '***'
-# CONSUMER_SECRET = '***'
-# ACCESS_TOKEN = '***'
-# ACCESS_TOKEN_SECRET = '***'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# https://www.makeuseof.com/python-bitly-url-shortener-create/
+def url_shortener(url):
+    endpoint = 'https://api-ssl.bitly.com/v4/shorten' 
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN_BITLY}',
+        'Content-Type': 'application/json',
+    }
 
-# ACCESS_TOKEN_BITLY = '***'
-bitly = bitly_api.Connection(access_token=ACCESS_TOKEN_BITLY)
-
-
-CHROME_PATH = '/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/mnt/various/BOTS/nba_scores_botaki/chromedriver'
-WINDOW_SIZE = '1920,1080'
-
-chrome_options = webdriver.chrome.options.Options()  
-chrome_options.add_argument('--headless')  
-chrome_options.add_argument('--window-size=%s' % WINDOW_SIZE)
-chrome_options.binary_location = CHROME_PATH
+    max_retries = 3
+    retry_count = 0
+    while retry_count < max_retries:
+        data = {'long_url': url}
+        response = requests.post(endpoint, headers=headers, data=json.dumps(data)) 
+        if response.status_code == 200:
+            shortened_url = json.loads(response.content)['link']
+            # print(f'Shortened URL: {shortened_url}')
+            return shortened_url
+            break
+        else:
+            retry_count += 1
+            if retry_count < max_retries:
+                time.sleep(5)
+    else:
+        print('URL shortening was not successful.')
 
 def find_between(s,first,last):
     try:
@@ -68,40 +76,67 @@ def create_tweet(result):
     play_by_play = base_url + '/nba/playbyplay/_/gameId/' + gameId
     gamecast = base_url + '/nba/game/_/gameId/' + gameId
 
-    base_bitly = 'es.pn/'
-    recap = 'Recap\t\t : ' + base_bitly + bitly.shorten(recap)['hash']
-    boxscore = 'Boxscore\t : ' + base_bitly + bitly.shorten(boxscore)['hash']
-    play_by_play = 'Play-By-Play\t : ' + base_bitly + bitly.shorten(play_by_play)['hash']
-    gamecast = base_bitly + bitly.shorten(gamecast)['hash']
+    # base_bitly = 'es.pn/'
+    # recap = 'Recap\t\t : ' + base_bitly + bitly.shorten(recap)['hash']
+    # boxscore = 'Boxscore\t : ' + base_bitly + bitly.shorten(boxscore)['hash']
+    # play_by_play = 'Play-By-Play\t : ' + base_bitly + bitly.shorten(play_by_play)['hash']
+    # gamecast = base_bitly + bitly.shorten(gamecast)['hash']
+    recap = 'Recap\t\t : ' + url_shortener(recap)
+    boxscore = 'Boxscore\t : ' + url_shortener(boxscore)
+    play_by_play = 'Play-By-Play\t : ' + url_shortener(play_by_play)
+    gamecast = url_shortener(gamecast)
     urls=''.join((recap, '\n', boxscore, '\n', play_by_play,'\n', hashtags, '\n', hashtags2, '\n', gamecast))
 
     tweet = ''.join((score,'\n',urls))
     return tweet    
     
-date = str(datetime.date.today()-datetime.timedelta(1)).split('-')
-latest_tweet_date = str(api.user_timeline(id = api.me().id, count = 1)[0].created_at)[0:10]
-todays_date = str(datetime.date.today())[0:10]
-if(latest_tweet_date == todays_date):
-    print('Already tweeted for today, tomorrow again!')
-    exit()
 
-urlpage = 'https://www.espn.com/nba/scoreboard/_/date/'+date[0]+date[1]+date[2]
+if __name__ == '__main__':
+    # CONSUMER_KEY = '***'
+    # CONSUMER_SECRET = '***'
+    # ACCESS_TOKEN = '***'
+    # ACCESS_TOKEN_SECRET = '***'
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    
+    # ACCESS_TOKEN_BITLY = '***'
+    # bitly = bitly_api.Connection(access_token=ACCESS_TOKEN_BITLY)
 
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-driver.get(urlpage)
-res = driver.execute_script('return document.documentElement.outerHTML')
-driver.quit()
+    CHROME_PATH = '/usr/bin/google-chrome'
+    CHROMEDRIVER_PATH = '/mnt/various/BOTS/nba_scores_botaki/chromedriver'
+    WINDOW_SIZE = '1920,1080'
 
-# parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(res,'lxml')
+    chrome_options = webdriver.chrome.options.Options()  
+    chrome_options.add_argument('--headless')  
+    chrome_options.add_argument('--window-size=%s' % WINDOW_SIZE)
+    chrome_options.binary_location = CHROME_PATH
 
-# find results within table
-table = soup.find('section',attrs={'class':'Card gameModules'})
-results = table.find_all('section',attrs={'class':'Scoreboard bg-clr-white flex flex-auto justify-between'})
 
-print(len(results),'NBA Games on ',date[1],'-',date[2],'-',date[0],'\n')
-for result in results:
-    reso1 = create_tweet(result)
-    print(reso1,'\n')
-    api.update_status(reso1)
-    time.sleep(10)
+    date = str(datetime.date.today()-datetime.timedelta(1)).split('-')
+    latest_tweet_date = str(api.user_timeline(id = api.me().id, count = 1)[0].created_at)[0:10]
+    todays_date = str(datetime.date.today())[0:10]
+    if(latest_tweet_date == todays_date):
+        print('Already tweeted for today, tomorrow again!')
+        exit()
+
+    urlpage = 'https://www.espn.com/nba/scoreboard/_/date/'+date[0]+date[1]+date[2]
+
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver.get(urlpage)
+    res = driver.execute_script('return document.documentElement.outerHTML')
+    driver.quit()
+
+    # parse the html using beautiful soup and store in variable 'soup'
+    soup = BeautifulSoup(res,'lxml')
+
+    # find results within table
+    table = soup.find('section',attrs={'class':'Card gameModules'})
+    results = table.find_all('section',attrs={'class':'Scoreboard bg-clr-white flex flex-auto justify-between'})
+
+    print(len(results),'NBA Games on ',date[1],'-',date[2],'-',date[0],'\n')
+    for result in results:
+        reso1 = create_tweet(result)
+        print(reso1,'\n')
+        api.update_status(reso1)
+        time.sleep(10)
